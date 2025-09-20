55251ccb555a0acd224a76fcb3a7148baa4383db
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-09-19 19:26:20 -0700

    Initial setup

---
+++ 210-lab-13.cpp
@@ -0,0 +1,8 @@
+// COMSC-210 | Lab 13 | Dainiz Almazan
+// IDE used: CLion
+
+// Modify your Lab 12 code such that it uses the STD::vector rather than the STD::array.
+
+int main(){
+    return 0;
+}
